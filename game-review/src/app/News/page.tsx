3f99a1fa087d40be54e159e8710bc5f7af594c98
jym126 <jym126@hotmail.com>
2023-12-15 07:29:49 +0100

    full

--- game-review/src/app/News/page.tsx
+++ game-review/src/app/News/page.tsx
@@ -3,6 +3,12 @@ import Cards from "../components/Cards";
 
 import { ClassNames } from "@emotion/react";
 
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
 export default async function Page() {
 
@@ -29,7 +35,38 @@ return (
   <div className="flex flex-wrap">
       {result.map(game => {
         return(
-          <Cards game = {game}/>
+          <Card key={game.id} game = {game} className="m-2 rounded-xl" sx={{ maxWidth: 345 }}>
+    <CardMedia
+      component="img"
+      alt='game cards'
+      height="140"
+      image ={game.background_image}
+    />
+    <CardContent>
+      <Typography gutterBottom variant="h5" component="div">
+        {game.name}
+      </Typography>
+      <Typography variant="body2" color="text.secondary">
+        {game.description_raw}
+      </Typography>
+      <Typography variant="body2" color="text.secondary" className="flex">
+        <text className="font-bold mr-2">Genre:</text> {game.genres[0].name}
+      </Typography>
+      <Typography variant="body2" color="text.secondary" className="flex">
+        <text className="font-bold mr-2">Platform:</text> {game.platform}
+      </Typography>
+      <Typography variant="body2" color="text.secondary" className="flex">
+        <text className="font-bold mr-2">Developer:</text> {game.developer}
+      </Typography>
+      <Typography variant="body2" color="text.secondary" className="flex">
+        <text className="font-bold mr-2">Release Date:</text> {game.released}
+      </Typography>
+    </CardContent>
+    <CardActions>
+      <Button size="small">Share</Button>
+      <Button size="small">Learn More</Button>
+    </CardActions>
+  </Card>
 
         )
       })
--- game-review/src/app/Search/page.tsx
+++ game-review/src/app/Search/page.tsx
@@ -7,6 +7,13 @@ import Cards from "../components/Cards";
 import Form from "../Form/page";
 import SearchForm from "../SearchForm/page";
 
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
 const CreateForm = () => {
 
   const valor = [];
@@ -64,7 +71,38 @@ const CreateForm = () => {
         <div className="flex justify-center">
           {valor.map((game) => {
             return (
-              <Cards game = {game}/>
+              <Card key={game.id} game = {game} className="m-2 rounded-xl" sx={{ maxWidth: 345 }}>
+              <CardMedia
+                component="img"
+                alt='game cards'
+                height="140"
+                image ={game.background_image}
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  {game.name}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  {game.description_raw}
+                </Typography>
+                <Typography variant="body2" color="text.secondary" className="flex">
+                  <text className="font-bold mr-2">Genre:</text> {game.genres[0].name}
+                </Typography>
+                <Typography variant="body2" color="text.secondary" className="flex">
+                  <text className="font-bold mr-2">Platform:</text> {game.platform}
+                </Typography>
+                <Typography variant="body2" color="text.secondary" className="flex">
+                  <text className="font-bold mr-2">Developer:</text> {game.developer}
+                </Typography>
+                <Typography variant="body2" color="text.secondary" className="flex">
+                  <text className="font-bold mr-2">Release Date:</text> {game.released}
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small">Share</Button>
+                <Button size="small">Learn More</Button>
+              </CardActions>
+            </Card>
             );
           })}
         </div>
