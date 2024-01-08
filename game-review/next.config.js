437f2d24b45a9b45dcadcc2872789d901e4841e7
jym126 <jym126@hotmail.com>
2024-01-08 15:22:56 +0100

    error fix 4

--- game-review/next.config.js
+++ game-review/next.config.js
@@ -3,12 +3,5 @@ const nextConfig = {}
 eslint: {
     ignoreDuringBuilds: true,
 },
-typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
 
 module.exports = nextConfig
